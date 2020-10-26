76c9ac8750e51215872e53b302d9f4acd14ebda0
LIJINGDI <lijingdi@lijingdioo.local>
2020-10-26 17:38:36 +0000

    add tests

---
+++ tests/download_data.sh
@@ -0,0 +1,7 @@
+# download example metagenome and related reads files for KM identification and quantification
+wget ftp://ftp.sra.ebi.ac.uk/vol1/ERZ926/ERZ926571/256318_metagenome.PRJEB18557.ERR1753690.contigs.fasta.gz -O genomes/ERR1753690_metagenome.fna.gz && gunzip genomes/ERR1753690_metagenome.fna.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR175/000/ERR1753690/ERR1753690_1.fastq.gz &&
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR175/000/ERR1753690/ERR1753690_2.fastq.gz
+
+
+
