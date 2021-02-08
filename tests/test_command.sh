0ffceac14afcd2d9d9dbb3428eca3a929f3dcb7b
lijingdi <2422498534@qq.com>
2021-02-08 22:46:01 +0000

    Update test_command.sh

--- tests/test_command.sh
+++ tests/test_command.sh
@@ -1,5 +1,5 @@
 ### run iqKM for KM identification only ###
-iqkm -i genomes/GCF_003018455.1_ASM301845v1_genomic.fna -o output_identification/ --help_dir ../help_dir/ -n 4 
+iqkm -i ./tests/genomes/GCF_003018455.1_ASM301845v1_genomic.fna -o tests/output_identification/ --help_dir Your_path_to_help_dir -n 4 
 # the example output folder structure, final KM identification summary is in KM_assignment_filtered
 drwxr-xr-x  10 lijingdi  staff   320B Oct 26 15:07 prodigal
 drwxr-xr-x   4 lijingdi  staff   128B Oct 26 16:53 hmmsearch
@@ -9,7 +9,7 @@ drwxr-xr-x   4 lijingdi  staff   128B Oct 26 16:58 KM_assignment_filtered
 
 
 ### run iqKM for both KM identification and quantification ###
-iqkm -i tests/genomes/ERR1753690_metagenome.fna -o tests/output_iqkm/ --help_dir ../iqKM_copy/help_dir --fq tests/reads/ERR1753690_1.fastq.gz --rq tests/reads/ERR1753690_2.fastq.gz --meta --quantify -n 4
+iqkm -i tests/genomes/ERR1753690_metagenome.fna -o tests/output_iqkm/ --help_dir Your_path_to_help_dir --fq tests/reads/ERR1753690_1.fastq.gz --rq tests/reads/ERR1753690_2.fastq.gz --meta --quantify -n 4
 # the example output folder structure, final KM identification summary is in KM_assignment_filtered
 # final KM abundance summary output is in out_abundance folder
 drwxr-xr-x  10 lijingdi  staff   320B Oct 26 15:07 prodigal
